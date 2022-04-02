a5ea71c3f23786ed4319ec05c4a4488cb3eff6b0
elroytoscano <andeltos@gmail.com>
2022-04-02 15:38:08 +0530

    added single and multiple entry points

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,4 @@
-(()=>{"use strict";console.log("Imported from other js file"),console.log("Hello World")})();
+(() => {
+  'use strict';
+  console.log('Imported from other js file'), console.log('Hello World');
+})();
